176944f92b1dc78890bb25f93d9a60cde5e89531
kirxda <lubentsovkd@mer.ci.nsu.ru>
2026-05-28 09:30:29 +0700

    Update AboutViewModel.cs
    fix error in author name

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -5,7 +5,7 @@ namespace laba9rpm3k._2.ViewModels
     public class AboutViewModel : ObservableObject, INavigationAware
     {
         private string _appName = "Телефонная книга MVVM";
-        private string _authors = "by me (kxrada) & Deepseek";
+        private string _authors = "by me (kirxda) & Deepseek";
 
         public string AppName
         {
